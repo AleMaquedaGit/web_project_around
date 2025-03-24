1e69a5f8db556fdb7dcf6a5458c52bb0a465bef3
AleMaquedaGit <alealemaqueda@gmail.com>
2025-03-24 08:28:32 -0600

    Actualizacion 24-03-24

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const openModal = document.querySelector(".userbutton_small");
+const openModal = document.querySelector(".user__button_edit");
 const modal = document.querySelector(".popup");
 
 const closeModal = document.querySelector(".popup__button-close");
@@ -6,8 +6,8 @@ const buttonDisable = document.querySelector(".popup__form-button");
 const nameInput = document.querySelector(".popup__form-name");
 const occupationInput = document.querySelector(".popup__form-occupation");
 const saveChange = document.querySelector(".popup__form-button");
-const usuario = document.querySelector(".username");
-const descripcion = document.querySelector(".userdescription");
+const usuario = document.querySelector(".user__name_paragraph");
+const descripcion = document.querySelector(".user__description_info");
 
 openModal.addEventListener("click", (e) => {
   console.log("funciona el click");
